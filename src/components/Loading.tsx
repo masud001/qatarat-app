d24ef45a12921e7ce95293ea990677c294c0eac7
masud001 <madhnagar@gmail.com>
2025-06-25 22:49:31 +0600

    update env var

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Loading: React.FC = () => {
   return (
     <div className="flex items-center justify-center w-full h-full py-10">
-      <div className="animate-spin rounded-full h-12 w-12 border-t-4 border-blue-600 border-opacity-75"></div>
+      <div className="animate-spin rounded-full h-12 w-12 border-t-4 border-[--active-text-color] border-opacity-75"></div>
       <span className="ml-4 text-gray-500 dark:text-gray-400 text-lg font-medium">Loading...</span>
     </div>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -3,16 +3,18 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
-
-if (import.meta.env.DEV) {
-  const { worker } = await import('./mocks/browser.ts');
-  await worker.start();
+async function enableMocking() {
+  // Enable MSW in development, or if VITE_ENABLE_MSW is set to "true"
+  if (import.meta.env.DEV || import.meta.env.VITE_ENABLE_MSW === "true") {
+    const { worker } = await import('./mocks/browser.ts');
+    await worker.start();
+  }
 }
+
+enableMocking();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    
-      <App />
-   
+    <App />
   </StrictMode>,
 )
