8c73d1daf698c8133d0a9f028b52a73af2588145
masud001 <madhnagar@gmail.com>
2025-06-26 09:00:30 +0600

    update

--- src/pages/Order.tsx
+++ src/pages/Order.tsx
@@ -1,8 +1,7 @@
 const Order = () => {
   return (
     <div
-      className="min-h-screen flex flex-col items-center justify-center transition-colors duration-300"
-      style={{ backgroundColor: 'var(--background-color)', color: 'var(--text-color)' }}
+      className="min-h-screen flex flex-col items-center justify-center transition-colors duration-300 bg-(--background-color) text-(--text-color) px-4"
     >
       <div className="bg-white  rounded-xl shadow-lg p-8 max-w-md w-full text-center border border-gray-200 ">
         <h1 className="text-2xl font-bold text-gray-900  mb-4">Order</h1>
